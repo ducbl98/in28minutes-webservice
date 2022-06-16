59b3b1a4029ac42333490430570013256cffeff5
duc.banh <duc.banh@bnksolution.com>
2022-06-16 18:49:31 +0700

    First Commit

---
+++ src/main/java/com/in28minutes/rest/webservices/resfulwebservices/helloworld/HelloWorldController.java
@@ -0,0 +1,36 @@
+package com.in28minutes.rest.webservices.resfulwebservices.helloworld;
+
+
+import com.in28minutes.rest.webservices.resfulwebservices.helloworld.HelloWorldBean;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+//Controller
+@RestController
+public class HelloWorldController {
+
+    //GET
+    //URI - /hello-world
+    //method - "Hello World"
+    @GetMapping(path = "/hello-world")
+    public String helloWorld() {
+        return "Hello World";
+    }
+
+    @GetMapping(path = "/hello-world-bean")
+    public HelloWorldBean helloWorldBean() {
+        return new HelloWorldBean("Hello World");
+    }
+
+    //hello-world/path-variable/in28minutes
+    @GetMapping(path = "/hello-world/path-variable/{name}")
+    public HelloWorldBean helloWorldPathVariable(@PathVariable String name) {
+        return new HelloWorldBean(String.format("Hello World, %s", name));
+    }
+
+    @GetMapping(path = "/hello-world-internationalized")
+    public String helloWorldInternationalization() {
+        return "Hello World";
+    }
+}
